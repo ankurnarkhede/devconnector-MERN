31c1eaea71d1b9331aba714ba489b9ea5537972b
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-12-29 19:46:39 +0530

    13] PASSPORT jwt AUTH

--- routes/api/users.js
+++ routes/api/users.js
@@ -97,7 +97,11 @@ router.post('/login', (req, res) => {
  */
 router.get('/current', passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    res.json(req.user)
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    })
   })
 
 module.exports = router
