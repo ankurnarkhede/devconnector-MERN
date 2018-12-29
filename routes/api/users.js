91d5aab74a57d816debb8d6b5220528f871f0b49
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-12-30 02:19:52 +0530

    15] validation of handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport')
 
 // load input validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 
 // load user model
@@ -68,6 +69,14 @@ router.post('/register', (req, res) => {
  *
  */
 router.post('/login', (req, res) => {
+
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   const email = req.body.email
   const password = req.body.password
 
@@ -76,7 +85,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if (!user) {
-        return res.status(404).json({ email: 'User not found' })
+        errors.email = 'User not found'
+        return res.status(404).json(errors)
       }
 
       // check password
@@ -97,7 +107,8 @@ router.post('/login', (req, res) => {
               })
             })
         } else {
-          return res.status(400).json({ password: 'Password incorrect' })
+          errors.password = 'Password incorrect'
+          return res.status(400).json(errors)
         }
       })
     })
--- validation/login.js
+++ validation/login.js
@@ -2,3 +2,31 @@
  * Created by ankur at 29/12/18 7:48 PM.
  */
 
+
+const Validator = require('validator')
+const isEmpty = require('./is-empty')
+
+module.exports = function validateLoginInput (data) {
+  let errors = {}
+
+  data.email = !isEmpty(data.email) ? data.email : ''
+  data.password = !isEmpty(data.password) ? data.password : ''
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email field is invalid'
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required'
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required'
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+
+}
