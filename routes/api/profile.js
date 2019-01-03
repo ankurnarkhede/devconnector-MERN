3c08b8a48d6a0acacb7f1ca3bc0f89e777345303
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-01-04 01:40:19 +0530

    21] getting profile by handle, user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -45,10 +45,42 @@ router.get(
  * @access Public
  *
  */
-router.get('/')
+router.get('/handle/:handle', (req, res) => {
+  const errors = {}
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err))
 
+})
 
+/**
+ * @route POST api/profile/user/:user_id
+ * @desc GET profile by user_id
+ * @access Public
+ *
+ */
+router.get('/user/:user_id', (req, res) => {
+  const errors = {}
 
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err))
+
+})
 
 /**
  * @route POST api/profile/
