908949b6d184aedc718da93b6205e0890f7a5890
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-01-06 10:58:55 +0530

    24] Post model

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+/**
+ * Created by ankur at 6/1/19 10:39 AM.
+ */
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+}, { timestamps: true })
+
+module.exports = Post = mongoose.model('post', PostSchema)
