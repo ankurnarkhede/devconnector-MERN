f7ac77f20d452510c7071ce7064dc5cae6bb5e79
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-01-14 23:37:12 +0530

    33] react router setup- basic routing

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ankur at 14/1/19 11:25 PM.
+ */
+
+import React, { Component } from 'react'
+
+class Login extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    )
+  }
+}
+
+export default Login
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ankur at 14/1/19 11:25 PM.
+ */
+
+import React, { Component } from 'react'
+
+class Register extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    )
+  }
+}
+
+export default Register
