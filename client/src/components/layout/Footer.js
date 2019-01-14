de250010f15ac88f3caf5717adcd4c86ffd9713d
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-01-14 23:12:01 +0530

    32] basic layout

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ankur at 14/1/19 10:34 PM.
+ */
+
+import React, { Component } from 'react'
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} DevConnector
+    </footer>
+  )
+}
