ae0b9bfa8f567d38bca20e4ac35dec33d84efb16
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-02-20 08:04:46 +0530

    46] profile reducer

--- client/src/App.js
+++ client/src/App.js
@@ -48,14 +48,14 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className='App'>
-            <Navbar />
-            <Route exact path='/' component={Landing} />
+            <Navbar/>
+            <Route exact path='/' component={Landing}/>
             <div className='container'>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
               <Route exact path='/dashboard' component={Dashboard}/>
             </div>
-            <Footer />
+            <Footer/>
           </div>
         </Router>
       </Provider>
