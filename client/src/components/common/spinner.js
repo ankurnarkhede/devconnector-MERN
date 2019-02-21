ab478fbc1b64ea3a4a66625365f5270f5ffa4730
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-02-21 16:53:18 +0530

    47] Spinner

---
+++ client/src/components/common/spinner.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ankur at 24/2/19 4:25 PM.
+ */
+
+import React from 'react'
+import spinner from './spinner.gif'
+
+export default () => {
+  return (
+    <div>
+      <img
+        src={spinner}
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+        alt="Loading..."
+      />
+    </div>
+  )
+}
--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -3,9 +3,11 @@
  */
 
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profileActions'
+import Spinner from '../common/spinner'
 
 class Dashboard extends Component {
   componentDidMount () {
@@ -13,12 +15,55 @@ class Dashboard extends Component {
   }
 
   render () {
+    const { user } = this.props.auth
+    const { profile, loading } = this.props.profile
+    let dashboardContent
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner/>
+    } else {
+      // check if logged inuser has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO::DISPLAY PROFILE</h4>
+      } else {
+        // user is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className='lead text-muted'> Welcome {user.name}</p>
+            <p>You have not yet setup a profile yet, please add some info</p>
+            <Link to='/create-profile' classname='btn btn-lg btn-info'>
+              Create Profile
+            </Link>
+          </div>
+        )
+      }
+
+    }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className='dashboard'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-4'>Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default connect(null, { getCurrentProfile })(Dashboard)
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
