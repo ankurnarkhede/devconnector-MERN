5a68dc327b74286ecabc1372afd9adbb27cff606
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-02-22 18:21:26 +0530

    Revert "48] PrivateRoute"
    This reverts commit 9ded0bd5

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import jwtDecode from 'jwt-decode'
 
@@ -8,7 +8,6 @@ import store from './store'
 import setAuthToken from './utils/setAuthToken'
 import { setCurrentUser, logoutUser } from './actions/authActions'
 import { clearCurrentProfile } from './actions/profileActions'
-import PrivateRoute from './components/common/PrivateRoute'
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
@@ -49,16 +48,14 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className='App'>
-            <Navbar/>
-            <Route exact path='/' component={Landing}/>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
             <div className='container'>
-              <Route exact path='/register' component={Register}/>
-              <Route exact path='/login' component={Login}/>
-              <Switch>
-                <PrivateRoute exact path='/dashboard' component={Dashboard}/>
-              </Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/dashboard' component={Dashboard} />
             </div>
-            <Footer/>
+            <Footer />
           </div>
         </Router>
       </Provider>
