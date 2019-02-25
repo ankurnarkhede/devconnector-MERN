966049cc905995be2ee149869cc4c4ef3fb7dcde
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-02-25 17:44:33 +0530

    50] Form field components

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -1,25 +1,28 @@
 /**
- * Created by ankur at 19/2/19 8:39 AM.
+ * Created by ankur at 25/2/19 5:06 PM.
  */
 
 import React from 'react'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
 
-const TextFieldGroup = ({
+const InputGroup = ({
   name,
   placeholder,
   value,
-  label,
   error,
-  info,
+  icon,
   type,
-  onChange,
-  disabled
+  onChange
 }) => {
   return (
-    <div className='form-group'>
-      <input type={type}
+    <div className='input-group mb-3'>
+      <div className='input-group-prepend'>
+        <span className='input-group-text'>
+          <i className={icon} />
+        </span>
+      </div>
+      <input
         className={classnames('form-control form-control-lg', {
           'is-invalid': error
         })}
@@ -27,27 +30,24 @@ const TextFieldGroup = ({
         name={name}
         value={value}
         onChange={onChange}
-        disabled={disabled}
       />
-      {info && <small className='form-text text-muted'>{info}</small>}
       {error && (<div className='invalid-feedback'>{error}</div>)}
     </div>
   )
 }
 
-TextFieldGroup.propTypes = {
+InputGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
-  info: PropTypes.string,
+  icon: PropTypes.string,
   error: PropTypes.string,
   type: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string
+  onChange: PropTypes.func.isRequired
 }
 
-TextFieldGroup.defaultProps = {
+InputGroup.defaultProps = {
   type: 'text'
 }
 
-export default TextFieldGroup
+export default InputGroup
