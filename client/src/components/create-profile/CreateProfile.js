785db0480dfe9bcd564ca5f992c8f5920d584bc4
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-02-26 18:37:25 +0530

    51] Create profile form

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -52,45 +52,45 @@ class CreateProfile extends Component {
       socialInputs = (
         <div>
           <InputGroup
-            placeholder="Twitter Profile URL"
-            name="twitter"
-            icon="fab fa-twitter"
+            placeholder='Twitter Profile URL'
+            name='twitter'
+            icon='fab fa-twitter'
             value={this.state.twitter}
             onChange={this.onChange}
             error={errors.twitter}
           />
 
           <InputGroup
-            placeholder="Facebook Page URL"
-            name="facebook"
-            icon="fab fa-facebook"
+            placeholder='Facebook Page URL'
+            name='facebook'
+            icon='fab fa-facebook'
             value={this.state.facebook}
             onChange={this.onChange}
             error={errors.facebook}
           />
 
           <InputGroup
-            placeholder="Linkedin Profile URL"
-            name="linkedin"
-            icon="fab fa-linkedin"
+            placeholder='Linkedin Profile URL'
+            name='linkedin'
+            icon='fab fa-linkedin'
             value={this.state.linkedin}
             onChange={this.onChange}
             error={errors.linkedin}
           />
 
           <InputGroup
-            placeholder="YouTube Channel URL"
-            name="youtube"
-            icon="fab fa-youtube"
+            placeholder='YouTube Channel URL'
+            name='youtube'
+            icon='fab fa-youtube'
             value={this.state.youtube}
             onChange={this.onChange}
             error={errors.youtube}
           />
 
           <InputGroup
-            placeholder="Instagram Page URL"
-            name="instagram"
-            icon="fab fa-instagram"
+            placeholder='Instagram Page URL'
+            name='instagram'
+            icon='fab fa-instagram'
             value={this.state.instagram}
             onChange={this.onChange}
             error={errors.instagram}
@@ -153,64 +153,64 @@ class CreateProfile extends Component {
                 />
 
                 <TextFieldGroup
-                  placeholder="Website"
-                  name="website"
+                  placeholder='Website'
+                  name='website'
                   value={this.state.website}
                   onChange={this.onChange}
                   error={errors.website}
-                  info="Could be your own website or a company one"
+                  info='Could be your own website or a company one'
                 />
                 <TextFieldGroup
-                  placeholder="Location"
-                  name="location"
+                  placeholder='Location'
+                  name='location'
                   value={this.state.location}
                   onChange={this.onChange}
                   error={errors.location}
-                  info="City or city & state suggested (eg. Boston, MA)"
+                  info='City or city & state suggested (eg. Boston, MA)'
                 />
                 <TextFieldGroup
-                  placeholder="* Skills"
-                  name="skills"
+                  placeholder='* Skills'
+                  name='skills'
                   value={this.state.skills}
                   onChange={this.onChange}
                   error={errors.skills}
-                  info="Please use comma separated values (eg.
-                    HTML,CSS,JavaScript,PHP"
+                  info='Please use comma separated values (eg.
+                    HTML,CSS,JavaScript,PHP'
                 />
                 <TextFieldGroup
-                  placeholder="Github Username"
-                  name="githubusername"
+                  placeholder='Github Username'
+                  name='githubusername'
                   value={this.state.githubusername}
                   onChange={this.onChange}
                   error={errors.githubusername}
-                  info="If you want your latest repos and a Github link, include your username"
+                  info='If you want your latest repos and a Github link, include your username'
                 />
                 <TextAreaFieldGroup
-                  placeholder="Short Bio"
-                  name="bio"
+                  placeholder='Short Bio'
+                  name='bio'
                   value={this.state.bio}
                   onChange={this.onChange}
                   error={errors.bio}
-                  info="Tell us a little about yourself"
+                  info='Tell us a little about yourself'
                 />
 
-                <div className="mb-3">
+                <div className='mb-3'>
                   <button
-                    type="button"
+                    type='button'
                     onClick={() => {
                       this.setState(prevState => ({
                         displaySocialInputs: !prevState.displaySocialInputs
                       }))
                     }}
-                    className="btn btn-light"
+                    className='btn btn-light'
                   >
                     Add Social Network Links
                   </button>
-                  <span className="text-muted">Optional</span>
+                  <span className='text-muted'>Optional</span>
                 </div>
                 {socialInputs}
-                <input type="submit" value="Submit"
-                       className='btn btn-info btn-block mt-4'/>
+                <input type='submit' value='Submit'
+                  className='btn btn-info btn-block mt-4' />
 
               </form>
 
