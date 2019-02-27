a01518b7d15ffd4c0a0b0aa9fc8f52096a44b227
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-02-28 02:03:57 +0530

    53] Profile actions component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -8,7 +8,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types'
 
 // GET current profile
@@ -45,6 +46,25 @@ export const createProfile = (profileData, history) => dispatch => {
         payload: err.response.data
       })
     )
+}
+
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This can not be undone!')) {
+    axios.delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      )
+  }
 }
 
 // Clear Profile
