c23375ead848c4978f636110b2d48a873ed0955a
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-03-10 21:09:27 +0530

    heroku build

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const morgan = require('morgan')
+const path=require('path')
 
 // include local files
 const keys = require('./config/keys')
@@ -36,6 +37,17 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
+
+// serve static assets if in production
+if(process.env.NODE_ENV==='production'){
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
